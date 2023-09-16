f3ca57d9c09c48bb80dad67e0ab7d95312e91a3a
Anderson Viana <anderson1565@live.com>
2023-09-15 21:36:08 -0300

    removendo headers desnecessários

--- becrowd_collector.py
+++ becrowd_collector.py
@@ -6,25 +6,6 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 
-headers = {
-    "authority": "www.beecrowd.com.br",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-language": "pt-BR,pt;q=0.9",
-    "cache-control": "no-cache",
-    "cookie": "judge=mk1t1hnntg982e403gdqtm4hn0; csrfToken=90c163adeb2626676e72e4270d2687a23d7399ad5ddd007f35019370055e8d093839ab8feb7472556b9dee757a065066c524b365d89a467e5a8ef56b2720a5fe; _gid=GA1.3.9988277.1694784447; cf_clearance=B07xhJUm0JXXxp07DXW9MgNlnJsCzTXzPe.44sLnO.g-1694784447-0-1-87ee3157.ee86e56d.3c023823-0.2.1694784447; _gat=1; _clck=11rd9s6|2|ff1|0|1353; _gali=container; _ga_2RSK6HJVYH=GS1.1.1694784447.1.1.1694784455.52.0.0; _ga=GA1.1.1756228126.1694784447; _clsk=1srr114|1694784455572|2|1|z.clarity.ms/collect",
-    "dnt": "1",
-    "pragma": "no-cache",
-    "sec-ch-ua": '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-}
-
-
 def parser_profile_data(html_content):
     soup = BeautifulSoup(html_content, "html.parser")
     avatar_photo = (
